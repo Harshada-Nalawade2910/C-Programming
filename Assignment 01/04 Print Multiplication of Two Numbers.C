1cbded98d18252d81c62021212857ee1e6ac2b71
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-16 19:36:24 +0530

    Add files via upload

---
+++ Assignment 01/04 Print Multiplication of Two Numbers.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int Num1 = 0 , Num2 = 0 , Ans = 0;
+
+    printf("\n Enter The Num1: ");
+    scanf("%d" , &Num1);
+    printf("\n Enter The Num2: ");
+    scanf("%d" , &Num2);
+
+        Ans= Num1 * Num2;
+
+    printf("\n The Multiplication Of Two Numbers Is %d", Ans);
+
+getch();
+return 0;
+}
