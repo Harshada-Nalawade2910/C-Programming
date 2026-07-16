61671f396f627875dbda78b53237a9d81a6ab9a8
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-16 20:21:30 +0530

    Add files via upload

---
+++ Assignment 01/14.Display Area Of Square.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+
+    int side = 0, Area = 0;
+
+    printf("\n Enter The side:");
+    scanf("%d" ,&side);
+
+    Area = side * side;
+
+    printf("\n The Area Of Square %d:" ,Area);
+
+getch();
+return 0;
+
+}
