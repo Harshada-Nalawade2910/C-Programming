7f4d2554b8a179caa5f35ed8f66630d4b036fb76
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-16 20:15:01 +0530

    Add files via upload

---
+++ Assignment 01/06. Print size of operators.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    printf("\n The size of char is %d" , sizeof(char));
+    printf("\n The size of int is %d" , sizeof(int));
+    printf("\n The size of float is %d" ,sizeof (float));
+    printf("\n The size of void is %d" ,sizeof (void));
+
+    getch();
+    return 0;
+
+}
