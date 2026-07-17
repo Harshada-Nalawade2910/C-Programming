02f968e450ce487ece695f17857a744138c1eb00
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 23:29:07 +0530

    Add files via upload

---
+++ Assignment 03/02.Print the numbers from 90 to 100 in  Desending Order (For Loop).C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    for(i = 100 ; i >= 91 ; i--)
+    {
+        printf("\n %d" , i);
+    }
+
+    getch();
+    return 0;
+
+}
