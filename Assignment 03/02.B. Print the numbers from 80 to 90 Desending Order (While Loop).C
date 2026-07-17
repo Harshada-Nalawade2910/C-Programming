acd72e824de5bac7fd6b53f3276d4a5d7875ec66
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 23:28:34 +0530

    Add files via upload

---
+++ Assignment 03/02.B. Print the numbers from 80 to 90 Desending Order (While Loop).C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num = 90;
+
+    while(num >= 80)
+    {
+        printf("\n %d" , num);
+        num--;
+    }
+
+    getch();
+    return 0;
+
+}
