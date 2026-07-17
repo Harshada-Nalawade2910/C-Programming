0671eb4d471e8ee79b90985922ea9b9b3f2f2a05
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 14:10:07 +0530

    Add files via upload

---
+++ Assignment 02/09.Accept a Month number and print it.C
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int month = 0;
+    printf("\n Enter the number of month:");
+    scanf("%d" ,&month);
+    {
+    if(month == 1)
+    printf("\n month is January");
+
+    else if(month == 2)
+    printf("\n month is February");
+
+    else if(month == 3)
+    printf("\n month is March");
+
+    else if(month == 4)
+    printf("\n month is April");
+
+    else if(month == 5)
+    printf("\n month is May");
+
+    else if(month == 6)
+    printf("\n month is june");
+
+    else if(month == 7)
+    printf("\n month is july");
+
+    else if(month == 8)
+    printf("\n month is August");
+
+    else if(month == 9)
+    printf("\n month is September");
+
+    else if(month == 10)
+    printf("\n month is october");
+
+    else if(month == 11)
+    printf("\n month is November");
+
+    else if(month == 12)
+    printf("\n month is December");
+
+    else
+    printf("\n not valid");
+    }
+
+getch();
+return 0;
+
+}
+
+
+
+
