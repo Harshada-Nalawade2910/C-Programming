343f60e845e3f99fe1f29c5c02db90b11581b8cb
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 23:27:57 +0530

    Add files via upload

---
+++ Assignment 03/01. B. Print a Number from 200 to 257(While Loop).C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 200;
+
+    while(No <= 257)
+    {
+        printf("\n %d" ,No);
+        No++;
+    }
+
+    getch();
+    return 0;
+
+
+}
