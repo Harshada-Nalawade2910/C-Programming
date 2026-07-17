f1af4bd8cdfdba549d4168b3b148cfddb669073f
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 21:49:59 +0530

    Add files via upload

---
+++ Assignment 03/01.A. Print a Numbers from 300 to 320(For Loop).C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    for(i = 300 ; i <= 320 ; i++)
+    {
+    printf("\n %d" ,i);
+    }
+
+    getch();
+    return 0;
+
+}
