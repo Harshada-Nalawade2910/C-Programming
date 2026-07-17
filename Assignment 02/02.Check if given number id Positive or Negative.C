9895c0bec94bca21efad91164452ce67070d3eac
Harshada-Nalawade2910 <harshdhanalawade@gmail.com>
2026-07-17 10:54:56 +0530

    Add files via upload

---
+++ Assignment 02/02.Check if given number id Positive or Negative.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num;
+
+    printf("\n Enter The Number:");
+    scanf("%d" ,&num);
+
+        if(num < 0)
+        {
+            printf("\n Number is Negative:");
+        }
+
+        else
+        {
+            printf("\n number is POsitive:");
+        }
+getch();
+return 0;
+
+}
