68b29ee2f31cc2c600f8559100bee3b9a0b311ee
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-07-27 12:00:07 +0530

    Add files via upload

---
+++ Assignment 04/01. Printing the smilies of fixed numbers.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= 6; i++)
+    {
+        for(j = 1; j <= 9; j++)
+        {
+            printf(" :) ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
