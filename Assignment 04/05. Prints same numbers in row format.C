adc95e5cd85a41c0e7726a9c0fa42f0c82732191
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-02 11:07:22 +0530

    Add files via upload

---
+++ Assignment 04/05. Prints same numbers in row format.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, R = 0, C = 0;
+
+    printf("\n Enter the row count =>");
+    scanf("%d",&R);
+    printf("\n Enter the column count =>");
+    scanf("%d",&C);
+
+    for(i = 1; i <= R; i++)
+    {
+        for(j = 1; j <= C; j++)
+        {
+            printf(" %d\t ", i);
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
