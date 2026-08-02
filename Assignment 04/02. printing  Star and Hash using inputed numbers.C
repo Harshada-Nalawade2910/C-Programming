176584f250d1618682b2a747e40d1d6e83d51efd
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-02 10:51:57 +0530

    Add files via upload

---
+++ Assignment 04/02. printing  Star and Hash using inputed numbers.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, R = 0, C = 0;
+
+    printf("\n Enter the row count =>");
+    scanf("%d",&R);
+    printf("\n Enter the column count =>");
+    scanf("%d",&C);
+
+    for(i = 1; i <= R; i++)
+    {
+        for(j = 1; j <= C; j++)
+        {
+            if(j % 2 == 1)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf(" # ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
