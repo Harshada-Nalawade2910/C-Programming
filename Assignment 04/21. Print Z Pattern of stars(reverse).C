ec5825856b097500966bfac0dd770678aa3598d7
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:51:15 +0530

    Add files via upload

---
+++ Assignment 04/21. Print Z Pattern of stars(reverse).C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, X = 0;
+
+    printf("\n Enter the count for printing");
+    scanf("%d",&X);
+
+    for(i = 1; i <= X; i++)
+    {
+        for(j = 1; j <= X; j++)
+        {
+            if(i == 1 || i == X || i == j)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
