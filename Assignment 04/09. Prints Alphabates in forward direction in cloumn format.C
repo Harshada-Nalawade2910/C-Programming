46e16bfba9fd31c05ce2b21fb2ef7794df708c9d
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:22:19 +0530

    Add files via upload

---
+++ Assignment 04/09. Prints Alphabates in forward direction in cloumn format.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, X = 0;
+    char ch = '\0';
+
+    printf("\n Enter the Count For printing =>");
+    scanf("%d",&X);
+
+    for(i = 1; i <= X; i++)
+    {
+        for(j = 1,ch = 'A'; j <= X; j++)
+        {
+            printf(" %c ",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
