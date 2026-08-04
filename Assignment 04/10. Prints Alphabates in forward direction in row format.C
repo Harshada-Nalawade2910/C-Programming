10493f069eea3818de4ee18698a3461a29b391f3
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:24:47 +0530

    Add files via upload

---
+++ Assignment 04/10. Prints Alphabates in forward direction in row format.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, X = 0;
+    char ch = 'A';
+
+    printf("\n Enter the Count For printing =>");
+    scanf("%d",&X);
+
+    for(i = 1; i <= X; i++)
+    {
+        for(j = 1; j <= X; j++)
+        {
+            printf(" %c ",ch);
+        }
+        ch++;
+
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
