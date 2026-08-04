a1b4fbb3ee9bb244c2a6ec80516f8d863b1e607a
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:50:50 +0530

    Add files via upload

---
+++ Assignment 04/20. Print Z Pattern of stars.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, X = 0;
+
+    printf("\n Enter the count => ");
+    scanf("%d",&X);
+
+    for(i = 1; i <= X; i++)
+    {
+        for(j = 1; j <= X; j++)
+        {
+            if(i == 1 || i == X || i + j == X + 1)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
