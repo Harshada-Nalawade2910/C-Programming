aca3d88f636311db5f3052cc5a14a71a9cd41f4b
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:46:29 +0530

    Add files via upload

---
+++ Assignment 04/15. Printing The stars of inputed numbers.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, X = 0;
+
+    printf("\n Enter the count for printing => ");
+    scanf("%d",&X);
+
+    for(i = 1; i <= X; i++)
+    {
+        for(j = 1; j <= X; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
