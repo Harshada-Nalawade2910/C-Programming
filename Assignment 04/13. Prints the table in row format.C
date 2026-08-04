d0d18eef4a1056cee33528c25d800388ad52b547
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-04 11:27:14 +0530

    Add files via upload

---
+++ Assignment 04/13. Prints the table in row format.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, R = 0, C = 0, num = 0;
+
+    printf("\n Enter the row count=>");
+    scanf("%d",&R);
+    printf("\n Enter the column count=>");
+    scanf("%d",&C);
+    printf("\n Enter the number for table printing=>");
+    scanf("%d",&num);
+
+    for(i = 1; i <= R; i++)
+    {
+        for(j = 1; j <= C; j++)
+        {
+            printf("%3d", num * ((i -1 )*C+j) );
+
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
