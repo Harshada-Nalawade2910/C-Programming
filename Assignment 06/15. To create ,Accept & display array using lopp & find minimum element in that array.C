9a12e321d30309b26049636ae92fdd8892b2b986
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-14 12:31:48 +0530

    Add files via upload

---
+++ Assignment 06/15. To create ,Accept & display array using lopp & find minimum element in that array.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0 , Num[10] = {}, MinNum = 0;
+
+    printf("\n Enter the 10 elements : \n");
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("\n The %d element : ",i+1);
+        scanf("%d",&Num[i]);
+
+        if(Num[i] < MinNum)
+        {
+            MinNum = Num[i];
+        }
+
+    }
+    printf("\n The Minimum Number between 10 numbers is %d",MinNum);
+
+    getch();
+    return 0;
+}
