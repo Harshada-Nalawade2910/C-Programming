73dee99b2eca95b976ef6e1cdad0bfc356c78257
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-14 11:57:32 +0530

    Add files via upload

---
+++ Assignment 06/03.To Create array of 7 elements with NULL initialization & display all elements(without lopp).C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i_a[7] = {};
+
+    printf("\n The value of 1st element => %d", i_a[0]);
+    printf("\n The value of 2nd element => %d", i_a[1]);
+    printf("\n The value of 3rd element => %d", i_a[2]);
+    printf("\n The value of 4th element => %d", i_a[3]);
+    printf("\n The value of 5th element => %d", i_a[4]);
+    printf("\n The value of 6th element => %d", i_a[5]);
+    printf("\n The value of 7th element => %d", i_a[6]);
+
+    getch();
+    return 0;
+}
+
