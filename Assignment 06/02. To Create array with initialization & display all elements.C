8312b19fdadec4cf7edd8a528d1e430c9d1e5983
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-14 11:56:07 +0530

    Add files via upload

---
+++ Assignment 06/02. To Create array with initialization & display all elements.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int iValues[7] = {57,90,45,897,234,56,9};
+
+    printf("\n The value of 1st element => %d", iValues[0]);
+    printf("\n The value of 2nd element => %d", iValues[1]);
+    printf("\n The value of 3rd element => %d", iValues[2]);
+    printf("\n The value of 4th element => %d", iValues[3]);
+    printf("\n The value of 5th element => %d", iValues[4]);
+    printf("\n The value of 6th element => %d", iValues[5]);
+    printf("\n The value of 7th element => %d", iValues[6]);
+
+    getch();
+    return 0;
+}
