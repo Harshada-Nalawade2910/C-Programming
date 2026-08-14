60a6930b9c25622b27d91bb824eba1c3067e9794
Harshada Nalawade <harshdhanalawade@gmail.com>
2026-08-14 11:52:13 +0530

    Add files via upload

---
+++ Assignment 06/01. To Create array without initialiazation & display all elements(without loop).C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int iValues[5] = {};
+
+    printf("\n The value of 1st element => %d", iValues[0]);
+    printf("\n The value of 2nd element => %d", iValues[1]);
+    printf("\n The value of 3rd element => %d", iValues[2]);
+    printf("\n The value of 4th element => %d", iValues[3]);
+    printf("\n The value of 5th element => %d", iValues[4]);
+
+    getch();
+    return 0;
+
+}
